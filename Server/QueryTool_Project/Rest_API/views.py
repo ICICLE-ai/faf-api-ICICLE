dd67c0b591e71b8c3b9ea679575c3e68bab40766
Chris Johnston <cj124@t-010.tempest.ise.luddy.indiana.edu>
2024-04-27 13:54:13 -0400

    queries now work

--- Server/QueryTool_Project/Rest_API/views.py
+++ Server/QueryTool_Project/Rest_API/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-from rest_framework.response import Response
+
+from rest_framework.response   import Response
 from rest_framework.decorators import api_view
+
 from src.Data_Lookup import QueryTool
+from .serializers import QuerySerializer
 
 @api_view(['GET'])
 def getData(request):
 
-    query = QueryTool()
-    query.connectSQL()
-    test = query.tables()
+    query  = QueryTool()
+    tables = query.show_db()
+    return Response(tables)
 
-    return Response(test)
+@api_view(['POST'])
+def getQuery(request):
+    serializer = QuerySerializer(data=request.data)
+    if serializer.is_valid():
+        query = serializer.validated_data['query']
+        
+        lookup = QueryTool()
+        data = lookup.query(query)
+        return Response(data)
+    else:
+        return Response(serializer.errors, status=400)
--- Server/QueryTool_Project/src/Data_Lookup.py
+++ Server/QueryTool_Project/src/Data_Lookup.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy import create_engine, MetaData, Table, text
 import src.DB_Mapping as m
+import json
 
 class QueryTool:
     def __init__(self, db='faf', usr='fafuser', psswrd='FQ^2t73Ava', host='localhost'):
@@ -11,12 +12,42 @@ class QueryTool:
         self.engine = None 
         self.conn   = None
 
+        self.connection  = f"mysql+pymysql://{self.usr}:{self.psswrd}@localhost:3306/{self.db}"
     def connectSQL(self):
-        connection  = f"mysql+pymysql://{self.usr}:{self.psswrd}@localhost:3306/{self.db}"
-        self.engine = create_engine(connection)
+        self.engine = create_engine(self.connection)
         self.conn   = self.engine.connect()
 
     def tables(self):
         return [x for x in m.table_dict.values()]
 
-    
+    def show_db(self):      
+        self.engine = create_engine(self.connection)
+        with self.engine.connect() as con:
+            sql_query = "SHOW TABLES;"
+
+            #seralize and map tables
+            r = con.execute(text(sql_query))
+            table_mapped = []
+            for tb in r:
+                try: table_mapped.append(m.table_dict[tb[0]])
+                except: table_mapped.append(tb[0])
+
+            return table_mapped
+
+    def query(self, string):
+        self.engine = create_engine(self.connection)
+        with self.engine.connect() as con:
+            
+            table = []
+            r = con.execute(text(string))
+            
+            #seralize to get all data from each entry
+            index = 0
+            for unit in r:
+                for spot in range(len(unit)):
+                    try: table[index].append(unit[spot])
+                    except: table.append([unit[spot]])
+                index +=1
+
+            return table
+        
